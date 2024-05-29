f54e62f110c0aaa4f62cfda0146dd2ca8219257f
Christhian Cunha <christian.cunha@bluebellindex.com>
2024-05-29 04:37:04 -0300

    Add design fiale

--- Home.py
+++ Home.py
@@ -8,15 +8,9 @@ import os
 import json
 
 
-json_data = st.secrets["json_data"] 
-service_account = st.secrets["service_account"]
-
-# Preparing values
-json_object = json.loads(json_data, strict=False)
-service_account = json_object['client_email']
-json_object = json.dumps(json_object)
-# Authorising the app
-credentials = ee.ServiceAccountCredentials(service_account, key_data=json_object)
+##linha de código para autenticação
+service_account = 'my-service-account@...gserviceaccount.com'
+credentials = ee.ServiceAccountCredentials(service_account, 'ee-scriptsremoteambgeo-040e397e0cc0.json')
 ee.Initialize(credentials)
 
 m=geemap.Map(height=800)
